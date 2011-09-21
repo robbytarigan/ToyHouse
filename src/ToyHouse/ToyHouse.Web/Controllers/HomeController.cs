f383132e04dad83e8131b10ce3bbb42f3fef1665
Robby Tarigan <robby.tarigan@gmail.com>
2011-09-21 20:34:38 +0100

    Move ToyHouse project.

--- src/ToyHouse/ToyHouse.Web/Controllers/HomeController.cs
+++ src/ToyHouse/ToyHouse.Web/Controllers/HomeController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Diagnostics;
 
 namespace ToyHouse.Web.Controllers {
     public class HomeController : Controller {
         public ActionResult Index() {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
 
+            
             return View();
         }
 
